5c8d75b81cdc31524b9b2e297d6e638982f50552
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-07 13:58:58 +0100

    Added /edit with completed checkbox

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit"
   post "tasks", to: "tasks#create"
 end
