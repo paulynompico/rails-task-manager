3221c0cc77ce882a02c0c109316c9caff058e50e
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-07 14:10:53 +0100

    Added #update

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks/:id/edit", to: "tasks#edit"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
   post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update"
 end
