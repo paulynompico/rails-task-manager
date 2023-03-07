450c5cf5101b0d777b6392f7294b1e62ca3c3966
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-07 13:34:40 +0100

    Linked tasks to task pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show"
+  get "tasks/:id", to: "tasks#show", as: :task 
 end
