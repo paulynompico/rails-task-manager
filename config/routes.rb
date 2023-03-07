b1289e03a2bc8a64e79c64d08926b8b5b31f7595
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-07 13:47:54 +0100

    Added /create with redirection

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new", as: :new_task
-  get "tasks/:id", to: "tasks#show", as: :task 
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
 end
